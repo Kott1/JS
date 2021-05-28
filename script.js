a8b98601d28b500b44e184640f560d6df2870735
Kott1 <mrkotto96@gmail.com>
2021-05-28 20:49:40 +0300

    ДЗ-1

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1-е задание--
+
+var Tc = prompt("Введите градусы по Фаренгейту:");
+var Tf = (9 / 5) * Tc + 32;
+alert(Tf)
+
+// 2-е задание--
+
+// var a = 7;
+// var b = 6;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// alert("a=" + a)
+// alert("b=" + b)
+
+// 3-е задание--(не вышло)
+
+// var Name = Василий;
+// var admin;
+// admin = Name;
+
+// alert(admin);
+
+// 4-е задание--
+
+// var a = 1000;
+// var b = "108";
+// c = a + b;
+
+// alert(c);
