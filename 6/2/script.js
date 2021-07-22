028054bb1a95ea57a4b203b36ff1d3c46a25d747
Kott1 <mrkotto96@gmail.com>
2021-07-22 16:18:59 +0300

    корзина

--- 6/2/script.js
+++ 6/2/script.js
@@ -1,23 +1,78 @@
-var goods = [
-    { title: 'shirt', price: 150 },
-    { title: 'socks', price: 50 },
-    { title: 'jacket', price: 350 },
-    { title: 'shoes', price: 250 },
-];
+var jacket = {
+    name: "Jacket",
+    price: 150,
+    src: "jacket.jpg"
+};
 
-var cart = document.querySelector(".cart");
-var buttons = document.querySelectorAll(".card__txt");
+var shirt = {
+    name: "Shirt",
+    price: 250,
+    src: "shirt.jpg"
+};
+
+var shoes = {
+    name: "Shoes",
+    price: 50,
+    src: "shoes.jpg"
+};
+
+var socks = {
+    name: "Socks",
+    price: 25,
+    src: "socks.jpg"
+};
+
+var goods = [jacket, shirt, shoes, socks];
+var sum = 0;
+
+function renderItems() {
+    var i, itemBlock;
+    var itemWrp = document.querySelector(".item_wrp");
+    for (i = 0; i < goods.length; i++) {
+        //div
+        itemBlock = document.createElement("div");
+        itemBlock.classList.add("item");
+        //img
+        var img = document.createElement("img");
+        img.src = "images/" + goods[i].src;
+        itemBlock.append(img);
+        //p
+        var pName = document.createElement("p");
+        pName.classList.add("item__txt");
+        pName.textContent = goods[i].name;
+        itemBlock.appendChild(pName);
+        //span
+        var pPrice = document.createElement("p");
+        pPrice.classList.add("item__price");
+        pPrice.textContent = goods[i].price;
+        itemBlock.appendChild(pPrice);
+        //button
+        var btn = document.createElement("button");
+        btn.classList.add("item__btn");
+        btn.id = "btn_" + i;
+        btn.textContent = "Buy";
+        btn.onclick = addToCart;
+        itemBlock.appendChild(btn);
+        //append to catalog
+        itemWrp.appendChild(itemBlock);
+    }
+}
+
+function addToCart(e) {
+    var button = e.target;
+    var id = button.id.split("_")[1];
+    var selectedItem = goods[id];
+    cartWrp = document.querySelector(".cart_wrp");
+    //div
+    var cartBlock = document.createElement("div");
+    //info
+    var cartInfo = document.createElement("span");
+    cartInfo.textContent = selectedItem.name + " " + selectedItem.price;
+    cartBlock.appendChild(cartInfo);
+    cartWrp.appendChild(cartBlock);
+
+    cartWrp.appendChild(document.createTextNode("    " + selectedItem.price));
+}
 
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', addToCart);
-});
-
-
-// function addToCart(event) {
-//     console.log(event.target);
-// }
-
-// buttons.forEach(button => {
-//     button.addEventListener('click', addToCart());
-// });
+window.onload = renderItems();
